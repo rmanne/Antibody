12c2d0189c92d1798ba1c4b0c75c4acde5069413
Rahul Manne <rahul_manne@hotmail.com>
2014-11-28 17:37:46 -0500

    Fixed the components

--- src/components.js
+++ src/components.js
@@ -21,21 +21,43 @@ Crafty.c("Shooter",{
 Crafty.c("PlayerCharacter",{
   init: function(){
     this.requires("Shooter, Fourway, spr_player")
-      .fourway(2);
-    //to add: collide with powerups, enemies, bullets, shooting
+      .fourway(2)
+      .stopOnSolids()
+      .onHit('Enemy', this.hit)
+      .onHit('Powerup', this.powerup);
+    //to add: collide with powerups, enemies, shooting
+    this.attr("health", 100);
+  },
+  hit: function(data) {
+    var health = this.attr("health");
+    if (health > 10) {
+      this.attr("health", health - 10);
+    } else {
+      this.attr("health", 0);
+      // TODO: die please
+    }
+  }
+  powerup: function(data) {
+    var powerobj = data[0].obj;
+    // TODO: patrick, what do we do with a powerup
+    powerobj.destroy();
   }
 });
 
 Crafty.c("Enemy",{
   init: function(){
-    this.requires("Shooter");
-    //to add: collide with player, bullets
+    this.requires("Shooter")
+      .stopOnSolids()
+      .onHit('PlayerCharacter', this.dispose);
+  },
+  dispose: function(data) {
+    this.destroy();
   }
 });
 
 Crafty.c("Powerup",{
   init: function(){
-    this.requires("Unit, Solid");
+    this.requires("Unit");
   }
   //to add: collide with player, time limit
 });
