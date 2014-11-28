a3e6f706f93f2b2b0d491eabe19e99f8b5cb85be
Patrick <pahdnguyen@yahoo.com>
2014-11-28 15:21:42 -0500

    Crafty.js

---
+++ src/components.js
@@ -0,0 +1,41 @@
+Crafty.c("Unit",{
+  init: function(){
+    this.requires('2D, Canvas');
+  },
+
+  at: function(x,y){
+    this.attr({x: x, y: y});
+  },
+
+  resize: function(w,h){
+    this.attr({w: w, h: h});
+  }
+});
+
+Crafty.c("Shooter",{
+  init: function(){
+    this.requires('Unit, Solid');
+  }
+});
+
+Crafty.c("PlayerCharacter",{
+  init: function(){
+    this.requires("Shooter, Fourway, spr_player")
+      .fourway(2);
+    //to add: collide with powerups, enemies, bullets, shooting
+  }
+});
+
+Crafty.c("Enemy",{
+  init: function(){
+    this.requires("Shooter");
+    //to add: collide with player, bullets
+  }
+});
+
+Crafty.c("Powerup",{
+  init: function(){
+    this.requires("Unit, Solid");
+  }
+  //to add: collide with player, time limit
+});
